df4d42a317b70a37a2ff33255291cedaa46e80c5
Garth Lilly <garff247@gmail.com>
2020-11-10 17:58:13 -0800

    added seeds to start making questions

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,421 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Question.create(question: "How many seasons of Friends are there?", answer1: "Ten Seasons", answer2: "Eight Seasons.", answer3: "Nine Seasons.", answer4: "Eleven Seasons.", correct_answer: "Ten seasons.")
+Question.create(question: "Joey played Dr. Drake Ramoray on which soap opera show?", answer1: "E.R.", answer2: "Baywatch.", answer3: "Days of Our Lives.", answer4: "The Golden Girls", correct_answer: "Days of Our Lives.")
+Question.create(question: "How many times did Ross get divorced?", answer1: "Two.", answer2: "Three.", answer3: "Four.", answer4: "Five.", correct_answer: "Three.")
+Question.create(question: "What store does Phoebe hate?", answer1: "Hobby Lobby.", answer2: "Bed, Bath and Beyond.", answer3: "Pottery Barn.", answer4: "", correct_answer: "Pottery Barn.")
+Question.create(question: "Rachel got a job with which company in Paris?", answer1: "Dior.", answer2: "Louis Vuitton.", answer3: "Macy's.", answer4: "Bloomingdale's.", correct_answer: "Louis Vuitton.")
+Question.create(question: "What are the names of Ross and Monica’s parents?", answer1: "David and Sarah Geller", answer2: "John and Judy Geller", answer3: "Jack and Jill Geller", answer4: "Jack and Judy Geller.", correct_answer: "Jack and Judy Geller.")
+Question.create(question: "What was the occupation of Rachel’s fiancé Barry Farber?", answer1: "Orthodontist.", answer2: "Dentist.", answer3: "Anesthesiologist.", answer4: "Radiologist", correct_answer: "Orthodontist.")
+Question.create(question: "Who was the maid of honor at Monica’s wedding?", answer1: "", answer2: "Rachel.", answer3: "", answer4: "", correct_answer: "Rachel.")
+Question.create(question: "Phoebe’s scientist boyfriend David worked in what city?", answer1: "", answer2: "", answer3: "Minsk.", answer4: "", correct_answer: "Minsk.")
+Question.create(question: "Monica dated an ophthalmologist named?", answer1: "Richard.", answer2: "", answer3: "", answer4: "", correct_answer: "Richard.")
+Question.create(question: "Which Sprouse brother played Ross’ son Ben?", answer1: "", answer2: "", answer3: "Cole.", answer4: "", correct_answer: "Cole.")
+Question.create(question: "True or false, Rachel kissed all of her Friends (male and female) on the show?", answer1: "True.", answer2: "False.", correct_answer: "True.")
+Question.create(question: "What is Chandler Bing’s middle name?", answer1: "", answer2: "", answer3: "", answer4: "Muriel.", correct_answer: "Muriel.")
+Question.create(question: "Who pees on Monica after she is stung by a jellyfish?", answer1: "", answer2: "Chandler.", answer3: "", answer4: "", correct_answer: "Chandler.")
+Question.create(question: "What is the name of Phoebe’s twin sister?", answer1: "Ursula.", answer2: "", answer3: "", answer4: "", correct_answer: "Ursula.")
+Question.create(question: "Ross says whose name at the altar in London?", answer1: "", answer2: "Rachel.", answer3: "", answer4: "", correct_answer: "Rachel.")
+Question.create(question: "Which member of the British royal family appeared on Friends?", answer1: "", answer2: "", answer3: "Sarah Ferguson/Duchess of York.", answer4: "", correct_answer: "Sarah Ferguson/Duchess of York.")
+Question.create(question: "Brad Pitt and David Schwimmer’s characters cofounded what club in high school?", answer1: "The 'Rachel Green Sucks Club.'", answer2: "The 'I Hate Rachel Green Club.'", correct_answer: "The 'I Hate Rachel Green Club.'")
+Question.create(question: "Which of Joey’s sisters did Chandler fool around with?", answer1: "Mary Angela.", answer2: "", answer3: "", answer4: "", correct_answer: "Mary Angela.")
+Question.create(question: "Who was Joey’s imaginary childhood friend? ", answer1: "Toby.", answer2: "Michael.", answer3: "Maurice.", answer4: "John.", correct_answer: "Maurice.")
+Question.create(question: "What was the profession of Joey’s imaginary friend?", answer1: "Space Pilot.", answer2: "Space Racer.", answer3: "Space Pirate.", answer4: "Space Cowboy.", correct_answer: "Space Cowboy.")
+Question.create(question: "What was Monica’s nickname when she was a field hockey goalie?", answer1: "Rachel Green's fat friend.", answer2: "Big Fat Goalie.", answer3: "Mon.", answer4: "", correct_answer: "Big Fat Goalie.")
+Question.create(question: "What does Rachel guess Chandler’s job is?", answer1: "", answer2: "A transponster.", answer3: "", answer4: "", correct_answer: "A transponster.")
+Question.create(question: "What is Richard’s daughter’s name?", answer1: "Michelle.", answer2: "Sarah.", answer3: "Susan.", answer4: "", correct_answer: "Michelle.")
+Question.create(question: "Who is Joey’s agent?", answer1: "Agnis.", answer2: "Stella.", answer3: "Sharon.", answer4: "Estelle.", correct_answer: "Estelle.")
+
+=begin 
+26. Question: Who mistakingly threw a woman’s wooden leg into a fire?
+Answer: Joey.
+
+27. Question: What was the name of Chandler and Ross’ college band?
+Answer: Way, No Way.
+
+28. Question: Monica categorizes her towels into how many categories?
+Answer: 11.
+
+29. Question: Ross and Rachel’s wedding dinner was held where in Vegas?
+Answer: A Pizza Hut.
+
+30. Question: Joey and Chandler’s TV guide is addressed to who?
+Answer: Miss Chanandler Bong.
+
+31. Question: What was the name of Ross’ pet monkey?
+Answer: Marcel.
+
+32. Question: According to Monica, a woman has how many erogenous zones?
+Answer: Seven.
+
+33. Question: What does Phoebe legally change her name to after her wedding?
+Answer: Princess Consulea Banana Hammock.
+
+34 Question: What form of self-defense does Ross try to teach Rachel and Phoebe?
+Answer: Unagi.
+
+35. Question: What song makes Ross and Rachel’s daughter Emma laugh for the first time?
+Answer: Sir Mix-a-Lot’s “Baby Got Back.”
+
+36. Question: What is the name of Joey’s stuffed penguin?
+Answer: Hugsy.
+
+37. Question: What caused the fire at Rachel and Phoebe’s apartment?
+Answer: Rachel’s hair straightener.
+
+38. Question: Phoebe is a surrogate for who?
+Answer: Her half-brother Frank Jr. and his wife Alice.
+
+39. Question: What color is Monica’s apartment?
+Answer: Purple.
+
+40. Question: How many sisters does Joey have?
+Answer: Seven.
+
+41. Question: Joey doesn’t share what?
+Answer: Food.
+
+42. Question: What do Monica and Chandler name their twins?
+Answer: Erica and Jack.
+
+43. Question: Ross’ first wife Carol leaves him for who?
+Answer: Susan Bunch.
+
+44. Question: What is the name of Chandler’s father’s Las Vegas all-male burlesque?
+Answer: Viva Las Gaygas.
+
+45. Question: Phoebe attempts to teach Joey what language?
+Answer: French.
+
+46. Question: Chick Jr. and Duck Jr. got stuck in what?
+Answer: A foosball table.
+
+47. Question: Who was married to a supposedly gay Canadian ice dancer named Duncan?
+Answer: Phoebe.
+
+48. Question: Rachel was in which sorority?
+Answer: Kappa Kappa Delta.
+
+49. Question: What are the names of Rachel’s sisters?
+Answer: Jill and Amy Greene.
+
+50: Question: Which character famously said, “PIVOT?”
+Answer: Ross.
+
+51. Question: Phoebe finds what in her soda can?
+Answer: A human thumb.
+
+52. Question: Who was (accidentally) Monica’s first kiss?
+Answer: Her brother Ross.
+
+53. Question: Rachel goes on Ross’ honeymoon by herself where?
+Answer: Athens, Greece.
+
+
+54. Question: What holiday does Chandler hate?
+Answer: Thanksgiving.
+
+55. Question: Who was Rachel’s prom date?
+Answer: Chip Matthews.
+
+56. Question: What was the name of Ross and Monica’s dog when they were kids?
+Answer: Chi-Chi.
+
+57. Question: What actor played Phoebe’s husband Mike Hannigan?
+Answer: Paul Rudd.
+
+58. Question: Where did Ross and Rachel have their first date?
+Answer: The planetarium.
+
+59. Question: How many roses did Ross send Emily?
+Answer: 72.
+
+60. Question: How many pages was Rachel’s letter to Ross?
+Answer: Eighteen pages (front and back).
+
+61. Question: What year did Friends first premiere?
+Answer: 1994.
+
+61. Question: Joey’s Cabbage Patch Kid is named what?
+Answer: Alicia May Emory
+
+62. Question: What was the name of the game Chandler made up to give Joey money?
+Answer: Cups.
+
+63. Question: In an effort to get over Richard, Monica started making what?
+Answer: Jam.
+
+64. Question: What fruit is Ross allergic to?
+Answer: Kiwi.
+
+65. Question: What is Rachel’s favorite flower?
+Answer: Lillies.
+
+66. Question: What was the name of Ross and Rachel’s male nanny?
+Answer: Sandy.
+
+67. Question: Who came on to who in The One With The Videotape?
+Answer: Rachel came on to Ross.
+
+68. Question: What was Joey’s nickname when he was working at Alessandro’s?
+Answer: Dragon.
+
+69. Question: Monica worked as a waitress at what diner?
+Answer: Moondance Diner.
+
+70. Question: What was Rachel’s bra size?
+Answer: 32C.
+
+71. Question: Ross worked as a professor at what school?
+Answer: New York University.
+
+72. Question: What word did Rachel misspell on her resume?
+Answer: Computer.
+
+73. Question: What are the names of the triplets Phoebe gives birth to?
+Answer: Frank Jr. Jr., Leslie and Chandler.
+
+74. Question: Who said, “See, he’s her lobster!”
+Answer: Phoebe.
+
+75. Question: What city is Friends set in?
+Answer: New York City.
+
+76. Question: What is the first wedding gift Monica opens?
+Answer: A tiny salt shaker.
+
+77. Question: Rachel’s sister Amy decides she wants to become what after babysitting Emma?
+Answer: A baby stylist.
+
+78. Question: Ross and Carol were busted having sex on which Disney ride?
+Answer: It’s a Small World.
+
+79. Question: Which Friend entered a Vanilla Ice lookalike contest and won?
+Answer: Chandler.
+
+80. Question: True or false, Monica ate macaroni off a jewelry box she made.
+Answer: True.
+
+81. Question: What ingredient did Rachel mistakingly put in her Thanksgiving trifle?
+Answer: Beef.
+
+82. Question: Phoebe thought “Kenny the Copy Guy” was who?
+Answer: Ralph Lauren.
+
+
+83. Question: What is the name of Joey’s barcalounger?
+Answer: Rosita.
+
+84. Question: Who dated a college student named Elizabeth Stevens?
+Answer: Ross.
+
+85. Question: Monica could not tell time until what age?
+Answer: 13.
+
+Parade Daily
+Celebrity interviews, recipes and health tips delivered to your inbox.
+
+Email Address
+ 
+86. Question: Phoebe created a three-dimensional picture of a woman named?
+Answer: Gladys.
+
+87. Question: Who was the last Friend to find out about Monica and Chandler’s relationship?
+Answer: Ross.
+
+88. Question: Chandler and Joey both dated an actress named?
+Answer: Kathy.
+
+89. Question: What did Monica want from Phoebe as an engagement present?
+Answer: Her grandmother’s cookie recipe.
+
+90. Question: Who had a pony and a boat at age 15?
+Answer: Rachel.
+
+91. Question: Phoebe said something was wrong with the right or the left phalange of Rachel’s plane?
+Answer: The left.
+
+92. Question: Who is Santa’s friend that Ross invents to teach Ben about Hanukkah?
+Answer: The Holiday Armadillo.
+
+93. Question: Who is the youngest Friend?
+Answer: Rachel.
+
+94. Question: Chandler told Janice he was moving where to avoid seeing her again?
+Answer: Yemen.
+
+95. Question: Who introduced Phoebe and Mike?
+Answer: Joey.
+
+96. Question: Phoebe’s grandmother said she got her cookie recipe from her grandmother named?
+Answer: Nestle Toulouse (AKA Nestlé Toll House).
+
+97. Question: The Geller Cup was a trophy for which sport?
+Answer: Touch football games.
+
+98. Question: Joey’s stalker Erika Ford was played by which actress?
+Answer: Brooke Shields.
+
+99. Question: Who did Monica forget to invite to Rachel’s baby shower?
+Answer: Rachel’s mother Sandra.
+
+100. Question: Which of Monica’s boyfriends wanted to become the Ultimate Fighting Champion?
+Answer: Pete (Peter Becker).
+
+101. Question: What was the name of the lipstick brand for men that Joey starred in a Japanese commercial for?
+Answer: Ichiban.
+
+102. Question: Ross, Susan and Phoebe got stuck where before Ben’s birth?
+Answer: A closet.
+
+103. Question: Who gave Phoebe away at her wedding?
+Answer: Chandler.
+
+104. Question: Monica and Chandler first got together where?
+Answer: London.
+
+105. Question: The handcuffs that were found in Rachel’s former room belonged to who?
+Answer: Monica and Ross’ Nana (grandmother).
+
+106. Question: Chandler and Ross helped Joey practice for what game show audition?
+Answer: Bamboozled.
+
+107. Question: What does Chandler call the Paris of Oklahoma?
+Answer: Tulsa.
+
+108. Question: What food caused Ross to get sick on Space Mountain?
+Answer: Tacos.
+
+109. Question: Who is older Ross or Monica?
+Answer: Ross.
+
+110. Question: What is actually Rachel’s favorite movie?
+Answer: Weekend at Bernie’s.
+
+111. Question: Which Friend had a traumatic swing incident at age 4?
+Answer: Rachel.
+
+112. Question: Ross and Rachel’s daughter Emma would have turned 18 what year?
+Answer: 2020.
+
+113. Question: Which Friend didn’t go to London for Ross’ wedding?
+Answer: Phoebe.
+
+114. Question: What kind of bed did the Mattress King deliver to Monica?
+Answer: A racecar bed.
+
+115. Question: Emma’s first birthday cake was supposed to be shaped like what?
+Answer: A bunny.
+
+116. Question: In what part of her body did Monica get a pencil stuck at age 14?
+Answer: Her ear.
+
+117. Question: Ross created a comic book called what?
+Answer: Science Boy.
+
+118. Question: What is Chandler’s surname?
+Answer: Bing.
+
+119. Question: What is Joey’s favorite food?
+Answer: Sandwiches.
+
+120. Question: How old was Chandler when he first touched a girl’s breast?
+Answer: 19.
+
+121. Question: What is Monica’s biggest pet peeve?
+Answer: Animals dressed as humans.
+
+122. Question: Which Friend had his/her identity stolen?
+Answer: Monica.
+
+123. Question: Joey doesn’t read which book without making sure there is plenty of room in the freezer?
+Answer: The Shining.
+
+124. Question: What is the name of Ross and Monica’s maternal grandmother?
+Answer: Althea.
+
+125. Question: Who stole all the insoles out of Chandler’s shoes?
+Answer: Chandler’s roomate Eddie.
+
+126. Question: Who gave Rachel and Ross the name “Emma” for their daughter?
+Answer: Monica.
+
+127. Question: Joey was cast as the butt double for which actor?
+Answer: Al Pacino.
+
+Related: A Friends Reunion Is Coming! HBO Max Confirms the Unscripted Special
+
+128.  Question: What is the name of Phoebe’s alter ego?
+Answer: Regina Phalange.
+
+
+129. Question: What year did the series finale of Friends air?
+Answer: 2004.
+
+130. Question: What is the name of Rachel and Monica’s building superintendent?
+Answer: Mr. Treeger.
+
+131. Question: What is Joey’s pin number?
+Answer: 5639.
+
+132. Question: What was the last state that Ross couldn’t remember?
+Answer: Delaware.
+
+133. Question: What was the name of Chandler’s roommate prior to Joey?
+Answer: Kip.
+
+134. Question: Whose idea was it to get married in Vegas, Rachel or Ross’?
+Answer: Rachel.
+
+135. Question: What was the name of Monica’s imaginary boyfriend?
+Answer: Jared.
+
+136. Question: What kind of plastic surgery did Rachel have?
+Answer: A nose job.
+
+137. Question: Who was called Sir Limps-a-Lot after losing a toe?
+Answer: Chandler.
+
+138. Question: What was the name of Rachel’s hairless cat?
+Answer: Mrs. Whiskerson.
+
+139. Question: Which balloon got away during the Macy’s Thanksgiving Day Parade in Season 1?
+Answer: Underdog.
+
+140. Question: Who gave birth to Chandler and Monica’s twins?
+Answer: Erica.
+
+141. Question: Chandler and Rachel ate cheesecake off the floor from what bakery?  
+Answer: Mama’s Little Bakery.
+
+142. Question: Which Friend once mugged Ross?
+Answer: Phoebe.
+
+143. Question: Monica, Ross and Rachel attended what high school?
+Answer: Lincoln High School.
+
+144. Question: Who were on “a break”?
+Answer: Rachel and Ross.
+
+145. Question: What was the name of Monica’s senior prom date?
+Answer: Roy Gublik.
+
+146. Question: Ross was jealous of who at Rachel’s job?
+Answer: Mark.
+
+147. Question: What was the name of the “hot girl from the Xerox place?”
+Answer: Chloe.
+
+148. Question: What did Rachel get a tattoo of?
+Answer: A heart.
+
+149. Question: Who is the first Friend to speak on the show?
+Answer: Monica.
+
+150. Question: Who sings the Friends theme song “I’ll Be There for You?”
+Answer: The Rembrandts.
+=end
