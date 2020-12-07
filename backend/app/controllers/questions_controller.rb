51e56184f3b23c3cf2a3381fcd51c34b735c3a4f
Garth Lilly <garff247@gmail.com>
2020-12-06 22:21:34 -0800

    created methods for QuestionsController

--- backend/app/controllers/questions_controller.rb
+++ backend/app/controllers/questions_controller.rb
@@ -1,2 +1,17 @@
 class QuestionsController < ApplicationController
+
+    def new
+        @user = Question.new
+    end
+
+    def create
+        @user = Question.new(question_params)
+    end
+
+    private
+
+    def question_params
+        params.require(:question).permit(:question, :answer1, :answer2, :answer3, :answer4, :correct_answer)
+    end
+
 end
