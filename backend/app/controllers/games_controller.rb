bba30345a0e8b511977d9556b1fc48bfba129ca3
Garth Lilly <garff247@gmail.com>
2020-12-06 22:23:11 -0800

    added methods to GamesController

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,2 +1,17 @@
 class GamesController < ApplicationController
+    
+    def new
+        @game = Game.new
+    end
+
+    def create
+        @game = Game.new(game_params)
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:user_id, :name, :topic)
+    end
+    
 end
